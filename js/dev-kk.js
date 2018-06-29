37f03e19e6dc47c4d835a3a0850d55866d0aa23c
kutalo-e <39828149+kutalo-e@users.noreply.github.com>
2018-06-29 12:37:35 +0300

    edit contacts-slide for main-page

---
+++ js/dev-kk.js
@@ -0,0 +1,61 @@
+(function ($) {
+    $(function() {
+        var $wrapper_dropdown = $('.wrapper-dropdown');
+        var $dd_span = $wrapper_dropdown.find('span');
+        var $dd_li = $wrapper_dropdown.find('li');
+
+        $wrapper_dropdown.on('click', function(event){
+            $(this).toggleClass('active');
+            return false;
+        });
+
+        $dd_li.on('click',function(){
+            var $this = $(this),
+                this_id = $this.data('id'),
+                $par = $this.closest('.contact-block'),
+                $city_down__item  = $('.city-down__item', $par),
+                $contact_ext  = $('.contact-ext', $par),
+                $contact_ext_target  = $('.contact-ext--'+this_id, $par);
+
+            $city_down__item.removeClass('city-down__item--active');
+            $this.addClass('city-down__item--active');
+
+            $contact_ext.removeClass('contact-ext--active');
+            $contact_ext_target.addClass('contact-ext--active');
+
+            $dd_span.html($this.html());
+        });
+    });
+    function contacts_padding() {
+        var windowWidth = window.innerWidth,
+            windowHeight = window.innerHeight,
+            $contacts_box = $('.contacts-box'),
+            $phone_block = $('.phone-block'),
+            $time_block = $('.time-block'),
+            $contact__map = $('.contact__map'),
+            $contact_block = $('.contact-block');
+
+
+        if (!$contacts_box.length) return;
+
+
+        $contacts_box.css('margin-top', '66px');
+        // $contacts_box.css('height', windowHeight*0.5+'px');
+        $contact__map.css('height', windowHeight*0.5+'px');
+        $phone_block.css('margin-bottom',  windowHeight*0.5*0.06+'px');
+        $time_block.css('margin-bottom',  windowHeight*0.5*0.13+'px');
+        // $time_block.css('margin-bottom',  windowHeight*0.5*0.13+'px');
+
+        if (windowWidth > 1080) {
+            $contact_block.css ('height', windowHeight+'px');
+            $contacts_box.css('margin-top', windowHeight*0.29+'px');
+        }
+    }
+    // установим обработчик события resize
+    $(window).resize(function(){
+        contacts_padding();
+    });
+    $(document).ready(function(){
+        contacts_padding();
+    });
+}($ || window.jQuery));
---
+++ template-parts/components/template-contact-ext.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * An example implementation of the component in code
+ *
+ * Using content of component in a custom implementation of the html
+ *
+ * @author Dmitry
+ * @version 0.01
+ * @package component
+ *
+ * COMPONENT IMPLEMENTATION: Контакты для главной
+ *
+ */
+
+global $Mammen;
+?>
+
+<?php
+$cities = $Mammen->get_fields( 'Города' );
+$first_city = '';
+if ( count( $cities ) ) {
+	foreach ( $cities as $city ) {
+		$first_city = $city->get_field( 'Название города' );
+		break;
+	}
+}
+?>
+<div class="section active" data-menuanchor="firstPage" id='zero'>
+	<div class="main ">
+        <div class="contact-block">
+            <div class="contacts-box">
+                <?php
+                if ( count( $cities ) ) {
+                    $j = 0;
+                    foreach ( $cities as $city ) {
+                        $j++;
+                        ?>
+                        <div class="center__left contact-ext contact-ext--<?php echo $j; ?> <?php echo ($j==1)?'contact-ext--active':''; ?>" data-id="<?php echo $j; ?>" >
+                                <div class="contact__map ">
+                                    <?php echo $city->get_field( 'Код карты' ); ?>
+                                </div>
+                            </div>
+                        <?php
+                    }
+                }?>
+
+                <div class="center__right">
+
+                    <div class="contacts-title">
+                        <h3 class="big-title f--65"><?php echo $Mammen->get_field( 'Название блока' ); ?></h3>
+                        <div id="dd" class="wrapper-dropdown wrapper-dropdown-1 city-down"><span><?php echo $first_city; ?></span>
+                            <ul class="dropdown city-down__ul">
+                                <?php
+                                if ( count( $cities ) ) {
+                                    $j = 0;
+                                    foreach ( $cities as $city ) {
+                                        $j++;
+                                        ?>
+                                        <li class="city-down__item <?php echo ($j==1)?'city-down__item--active':''; ?>" data-id="<?php echo $j; ?>" > <?php echo $city->get_field( 'Название города' ); ?> </li>
+                                        <?php
+                                    }
+                                }
+                                ?>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <?php
+                    if ( count( $cities ) ) {
+                        $j = 0;
+                        foreach ( $cities as $city ) {
+                            $j++;
+                            ?>
+                            <div class="contact-ext contact-ext--<?php echo $j; ?> <?php echo ($j==1)?'contact-ext--active':''; ?>" data-id="<?php echo $j; ?>" >
+                                <div class="center__right__block">
+                                    <p class="text--bold f--24"><?php echo $city->get_field( 'Адрес' ); ?></p>
+                                </div>
+                                <div class="center__left__time time-block">
+                                    <p class="f--16"><?php echo $city->get_field( 'Время работы' ); ?></p>
+                                </div>
+                                <div class="center__right__block phone-block">
+                                    <?php
+                                    $phones = $city->get_fields( 'Наши телефоны' );
+                                    if ( count( $phones ) ) {
+                                        foreach ( $phones as $phone ) {
+                                            ?>
+                                            <p class="text--bold f--30"><a
+                                                    href="tel:<?php echo tbs_clear_phone( $phone->get_field( 'Телефон' ) ); ?>"><?php echo $phone->get_field( 'Телефон' ); ?></a>
+                                            </p>
+                                            <?php
+                                        }
+                                    }
+                                    ?>
+                                </div>
+
+                                <div class="center__right__block ">
+                                    <p class="text--italic contact__email"><a
+                                            href="mailto:<?php echo $city->get_field( 'Email' ); ?>"><?php echo $city->get_field( 'Email' ); ?></a>
+                                    </p>
+                                </div>
+                            </div>
+                            <?php
+                        }
+                    }?>
+
+                </div>
+            </div>
+            <div class="contacts-footer">
+                <div class="contacts-footer__item copyright">© ThaiBeautySpa 2012-2018. Все права защищены.</div>
+                <div class="contacts-footer__item politika"><a href="">Политика конфиденциальности</a></div>
+            </div>
+        </div>
+	</div>
+</div>
